61d0d969da200fe99acd77cba181bf1777f7c747
Moussa <moussaebrahem32@gmail.com>
2021-08-21 15:21:05 +0200

    init app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    // BEM naming convention
+    <div className="app">
+      <h1>Let's build facebook clone</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyABGbB4CrZEp9necAPXOqYtWr_7LwIlt20",
+  authDomain: "facebook-clone-e5d99.firebaseapp.com",
+  projectId: "facebook-clone-e5d99",
+  storageBucket: "facebook-clone-e5d99.appspot.com",
+  messagingSenderId: "484062369465",
+  appId: "1:484062369465:web:c9f7eda8ae9750aa50ed68",
+};

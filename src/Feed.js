3c5fc39ec082b1a8e978bddeb346f6c909f5cfc5
Moussa <moussaebrahem32@gmail.com>
2021-08-25 13:59:44 +0200

    Added post component

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,7 @@
 import React from "react";
 import StoryReal from "./StoryReal";
 import MessageSender from "./MessageSender";
+import Post from "./Post";
 import "./Feed.css";
 
 const Feed = () => {
@@ -8,7 +9,18 @@ const Feed = () => {
     <div className="feed">
       <StoryReal />
       <MessageSender />
-      {/* MessageSender */}
+
+      <Post
+        key={1}
+        profilePic="https://assets.entrepreneur.com/content/3x2/2000/20190502194704-ent19-june-editorsnote.jpeg"
+        message="ازيكم يا شباب؟"
+        timestamp="1/12/2021"
+        username="Moussa Ibrahiem"
+        image="https://assets.entrepreneur.com/content/3x2/2000/20190502194704-ent19-june-editorsnote.jpeg"
+      />
+      <Post />
+      <Post />
+      <Post />
     </div>
   );
 };
